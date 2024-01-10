ac3429c1d858d50390ecad6eb1067f67f1a68c1a
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-10 07:45:44 -0800

    testing main branch rule

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <span>Testing GitHub branch rule</span>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
