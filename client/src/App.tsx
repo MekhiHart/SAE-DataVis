53646383dc4da0c5bf807e8cef329b6f66ebad81
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-10 07:48:15 -0800

    added test branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <span>Testing GitHub branch rule</span>
+        <span>Testing test branch to merge to main</span>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
