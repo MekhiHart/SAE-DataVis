aac9872447b425cffebe33e864f9fd066645890f
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-10 17:08:41 -0800

    changed modified file in server and exported s3client from db config file

--- server/src/config/dbS3.config.js
+++ server/src/config/dbS3.config.js
@@ -5,10 +5,12 @@ dotenv.config()
 
 
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
 // Set the region 
 AWS.config.update({region: process.env.AWS_DEFAULT_REGION});
 
 // Create S3 service object
-s3_Client = new AWS.S3({apiVersion: process.env.AWS_API_VERSION});
+const s3Client = new AWS.S3({apiVersion: process.env.AWS_API_VERSION});
+
+export default s3Client
---
+++ server/src/controllers/dbS3.controllers.js
@@ -0,0 +1,106 @@
+class DBS3Controller{
+    constructor(){
+        if (DBS3Controller._instance){
+            throw new Error("DBS3Controller class can't be instantiated more than once")
+        }
+        DBS3Controller._instance = this
+    }
+
+    // called in Upload View
+    CreateRaceFolder(){
+        const bucketName = process.env.AWS_MAIN_BUCKET
+        const key = "race_2/"
+
+        const metadata = {
+            'x-amz-meta-id': "12",
+            'x-amz-meta-name': "Irving Race",
+            'x-amz-meta-date': Date(),
+            'x-amz-meta-time': "60",
+            // Add more metadata key-value pairs as needed
+        };
+
+        const params ={
+            Bucket: bucketName,
+            Key: key,
+            Metadata: metadata,
+        }
+
+        s3.putObject(params, (err, data) =>{
+            if (err) {
+                console.error('Error uploading to S3:', err);
+            } else {
+                console.log('File uploaded successfully to:', data.Location);
+            }
+        })
+    } // createRaceFolder
+
+    // Called in Home View
+    GetRaceLogs(){
+        // Create the parameters for calling listObjects
+        const bucketName = process.env.AWS_MAIN_BUCKET
+        const bucketParams = {
+            Bucket : bucketName,
+        };
+        
+        // Call S3 to obtain a list of the objects in the bucket
+        s3.listObjects(bucketParams, function(err, data) {
+            if (err) {
+            console.log("Error", err);
+            } else {
+            console.log("Success", data);
+            }
+        });
+    } // listRaceLogs
+
+    // called in Subsytem View
+    GetRaceFolderContents(){
+        // Specify the bucket name and the prefix (folder path) in S3
+        const bucketName = process.env.AWS_MAIN_BUCKET;
+        const prefix = 'race_2';
+
+        // List the contents of the folder in S3
+        const params = {
+        Bucket: bucketName,
+        Prefix: prefix,
+        };
+
+        s3.listObjectsV2(params, (err, data) => {
+        if (err) {
+            console.error('Error listing S3 contents:', err);
+        } else {
+            console.log('Contents of the folder in S3:', data.Contents);
+        }
+        });
+    }
+
+    // called in Graph View
+    GetObject(){
+        const bucketName = process.AWS_MAIN_BUCKET
+        const key = "race_2/"
+
+        const params ={
+            Bucket: bucketName,
+            Key: key,
+        }
+
+        s3.getObject(params, (err, data) =>{
+            if (err) {
+                console.error('Error uploading to S3:', err);
+            } else {
+                console.log("data retrieved: ", data.Metadata);
+            }
+        })
+    } // GetObject
+
+    // called in Graph View
+    ParseObjectToJSON(data){
+        dataBuffer = data.Body
+        jsonObject = JSON.parse(dataBuffer.toString("utf-8"))
+        console.log("json: ", jsonObject)
+    } // ParseObjectToJSON
+
+} // DBS3Controller
+
+const test = new DBS3Controller()
+
+test.GetRaceLogs()
