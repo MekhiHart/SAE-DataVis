06dafb6ffad1ed25b28ce4ea46b2625cadf42cb3
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-11 18:57:21 -0800

    added css changes to layout

--- client/src/pages/Home/Components/RaceLogList.tsx
+++ client/src/pages/Home/Components/RaceLogList.tsx
@@ -1,6 +1,7 @@
 import { HomeViewNS } from "../../../utils/namespace/HomeNS"
 import RaceLog from "./RaceLog"
 
+
 interface RaceLogListProps{
     RaceLogListData: HomeViewNS.RaceLog["RaceLogData"][]
 }
@@ -8,7 +9,7 @@ export default function RaceLogList(props:RaceLogListProps){
     const raceFolders = props.RaceLogListData
 
     return(
-        <div>
+        <div id="raceloglist">
             {raceFolders.map((obj) => obj && <RaceLog key={obj.id} RaceLogData={obj}/>)}
         </div>
     )
--- client/src/pages/Layout/Layout.tsx
+++ client/src/pages/Layout/Layout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
 export default function Layout(){
     return (
-        <div>
+        <div >
             <div style={{backgroundColor:"aqua"}}>Nav Bar</div>
-            <Outlet/>
+
+            <div id="layout">
+                <Outlet/>
+            </div>
+            
         </div>
     )
 }
