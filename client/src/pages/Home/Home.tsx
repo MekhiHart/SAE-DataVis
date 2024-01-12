a46d7e3a23d5f13554e4441c0ef009438cafd00b
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-11 20:24:05 -0800

    added upload race button

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,12 +1,27 @@
 import { useGetRaceFolders } from "../../hooks/useGetRaceFolders";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload, faFileArrowUp } from "@fortawesome/free-solid-svg-icons";
 import RaceLogList from "./components/RaceLogList";
 export default function HomeView(){
 
     const {raceFolders} = useGetRaceFolders()
     return(
         <>
-            <h2 style={{color:"black"}}>Race Logs</h2>
+            <div style={{display:"flex"}}>
+                <h2 style={{color:"black"}}>Race Logs</h2>
+                <Upload/>
+            </div>
             <RaceLogList RaceLogListData={raceFolders} />
         </>
     )
+}
+
+const Upload = () =>{
+    return(
+    <div style={{display:"flex", color:"black", alignItems:"center", marginLeft:"auto"}}>
+        <FontAwesomeIcon size="2x" style={{color:"black", marginRight:"10", scale:"75%"}} icon={faFileArrowUp} />
+        <h4>Upload Race</h4>
+    </div>
+    )
+
 }
--- client/src/pages/Layout/components/NavBar.tsx
+++ client/src/pages/Layout/components/NavBar.tsx
@@ -1,9 +1,11 @@
+import { faUpload } from "@fortawesome/free-solid-svg-icons"
 import saeIcon from "../../../assets/SAEIcon.png"
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 export default function NavBar(){
     return(
         <div id="navbar">
             <img id="website--icon" src={saeIcon} />
-            <h2 id="website--title">SAE Data Visualizer</h2>
+            <h2 id="website--title">SAE Race Data Visualizer</h2>
         </div> 
     )
 }
