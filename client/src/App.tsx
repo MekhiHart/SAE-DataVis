f66a0226727e07bc7ae251783af816d74d9af428
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-11 18:59:50 -0800

    removed import statement in App file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home/Home'
 import Layout from "./pages/Layout/Layout";
-import './App.css'
 
 
 function App() {
