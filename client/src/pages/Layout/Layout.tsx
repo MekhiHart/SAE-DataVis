99c7aced73522560980e85c1cf55eb27ce2489ee
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-11 21:03:24 -0800

    added footer

--- client/src/pages/Layout/Layout.tsx
+++ client/src/pages/Layout/Layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
 
 export default function Layout(){
     return (
         <div >
             <NavBar/>
-
             <div id="layout">
                 <Outlet/>
             </div>
+            <Footer/>
             
         </div>
     )
---
+++ client/src/pages/Layout/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+
+    return(
+    <div id="footer">
+        <h3>Developed by the Data Acquisition Team</h3>
+    </div>       
+    )
+
+}
