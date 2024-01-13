fd84cf38d2dcb6832f3ded279ffe1b9cde4cae3e
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-13 15:49:54 -0800

    added useEffect and fetching graph data for Graph view

--- client/src/pages/Graph/Graph.tsx
+++ client/src/pages/Graph/Graph.tsx
@@ -4,13 +4,28 @@ import { useEffect, useState } from "react";
 export default function Graph(){
     const location = useLocation()
     const [graphData, setGraphData] = useState()
+    const bucketKey = location.state && location.state.bucketKey
+
 
     useEffect(() => {
-        const fetchData = () =>{
-            const data = fetch()
+        const fetchData = async () =>{
+            const url = "http://localhost:8000/api/getGraphJSON/"
+            const data = await fetch(url,{
+                method: "POST",
+                mode: "cors",
+                credentials: "same-origin",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                body: JSON.stringify({bucket_key:bucketKey})
+            })
+            const json = await data.json()
+            console.log("data: ", json)
         }
+        fetchData()
     },[])
-    const bucketKey = location.state && location.state.bucketKey
     return(
         <div>
             <h2>{bucketKey}</h2>
