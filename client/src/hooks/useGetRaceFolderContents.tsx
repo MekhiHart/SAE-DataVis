bf10aadb9700c764ec7141b5aaf20fec5f134a91
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-13 15:44:24 -0800

    extracted fetch logic from useGetRaceFodlerContents and integrated it into dbs3Controller

--- client/src/hooks/useGetRaceFolderContents.tsx
+++ client/src/hooks/useGetRaceFolderContents.tsx
@@ -1,24 +1,13 @@
 
 import { useEffect, useState } from "react"
 import { Interfaces } from "../utils/namespaces/Interfaces"
+import { dbs3Controller } from "../services/api/dbs3Controller"
 
 export const useGetRaceFolderContents = (bucketKey:string) =>{
     const [subsytemLogs, setSubsystemLogs] = useState<Interfaces.ISubsystem["SubsystemData"][]>([])
     useEffect(() => {
         const fetchData = async () =>{
-            const url = "http://localhost:8000/api/getRaceFolderContents/"
-            const data = await fetch(url, {
-                method: "POST",
-                mode: "cors",
-                credentials: "same-origin",
-                headers:{
-                    "Content-Type": "application/json"
-                },
-                redirect: "follow",
-                referrerPolicy: "no-referrer",
-                body: JSON.stringify({bucket_key:bucketKey})
-            })
-            const json = await data.json()
+            const json = await dbs3Controller.GetRaceFolderContents(bucketKey)
             setSubsystemLogs(json)
         }
     
--- client/src/services/api/dbs3Controller.ts
+++ client/src/services/api/dbs3Controller.ts
@@ -6,6 +6,23 @@ class DBS3Controller{
         const res: Interfaces.IRaceLog["RaceLogData"][] = await data.json()
         return res
     }
+
+    async GetRaceFolderContents(bucketKey: string): Promise<Interfaces.ISubsystem["SubsystemData"][]>{
+        const url = "http://localhost:8000/api/getRaceFolderContents/"
+        const data = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+            credentials: "same-origin",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify({bucket_key:bucketKey})
+        })
+        const json: Interfaces.ISubsystem["SubsystemData"][] = await data.json()
+        return json
+    }
 }
 
 export const dbs3Controller = new DBS3Controller()
