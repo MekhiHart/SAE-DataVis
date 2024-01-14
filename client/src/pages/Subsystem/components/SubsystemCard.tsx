21caa5baf90ba2ef3da67daf531ae0a6ff754ee0
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-14 12:23:24 -0800

    added shadows

--- client/src/pages/Subsystem/components/SubsystemCard.tsx
+++ client/src/pages/Subsystem/components/SubsystemCard.tsx
@@ -9,7 +9,7 @@ interface SubsystemCardProps{
 export default function SubsystemCard(props:SubsystemCardProps){
     const {subsystem, key} = props.SubsystemCardData
     return(
-        <Link to="/analysis" state={{bucketKey: key}} className="subsystem--card">
+        <Link to="/analysis" state={{bucketKey: key}} className="subsystem--card shadows">
             <SubsystemIcon subsystemName={subsystem} />
             <h4 className="subsystem--title">{subsystem}</h4>
         </Link>
