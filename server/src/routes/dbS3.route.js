f5b388b6d476098ecbb96f623fa2414f35904ba2
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-01-15 21:47:29 -0800

    removed console log

--- server/src/routes/dbS3.route.js
+++ server/src/routes/dbS3.route.js
@@ -7,7 +7,6 @@ const dbs3Controller = require("../controllers/dbS3.controllers")
 router
     .route("/getAllRaces")
     .get(async (req, res) => {
-        console.log("req: ", req.body)
         const foldersArr = await dbs3Controller.GetRaceFolders()
         const S3ObjArr = foldersArr.map(folder => dbs3Controller.GetMetaData(folder))
         const metaDataArr = await Promise.all(S3ObjArr)
@@ -32,7 +31,6 @@ router
         } 
         
         else{
-            console.log("called here")
             res.json(false)
         } // else
 
