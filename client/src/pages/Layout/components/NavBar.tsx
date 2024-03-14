6712b6da9bacf4f37961e0c0844b01d00130b101
Jaimin Vyas <jaiminenter@gmail.com>
2024-03-14 14:51:39 -0700

    Changes: 1). On Click of SAE Icon, redirect back to home page. 2). Change the text to X-Axis heading as 'Percentage of Race completed'.

--- client/src/pages/Layout/components/NavBar.tsx
+++ client/src/pages/Layout/components/NavBar.tsx
@@ -1,10 +1,14 @@
-import saeIcon from "../../../assets/SAEIcon.png"
-import "../index.css"
+import saeIcon from "../../../assets/SAEIcon.png";
+import "../index.css";
+import { useNavigate } from 'react-router-dom';
+
 export default function NavBar(){
+    const navigate = useNavigate();
+
     return(
-        <div id="navbar">
-            <img id="website--icon" src={saeIcon} />
-            <h2 id="website--title">SAE Race Data Visualizer</h2>
+        <div id="navbar" onClick={() => {navigate("/");}}>
+            <img id="website--icon" src={saeIcon} onClick={() => {navigate("/");}}/>
+            <h2 id="website--title" onClick={() => {navigate("/");}}>SAE Race Data Visualizer</h2>
         </div> 
     )
 }
