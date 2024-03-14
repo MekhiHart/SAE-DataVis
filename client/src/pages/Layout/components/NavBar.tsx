7501b22da0cb01d894896d2bb4ebbb8053420f4e
Jaimin Vyas <jaiminenter@gmail.com>
2024-03-14 15:06:16 -0700

    Styled the Navbar, for icon and title of the SAE.

--- client/src/pages/Layout/components/NavBar.tsx
+++ client/src/pages/Layout/components/NavBar.tsx
@@ -2,13 +2,20 @@ import saeIcon from "../../../assets/SAEIcon.png";
 import "../index.css";
 import { useNavigate } from 'react-router-dom';
 
-export default function NavBar(){
+export default function NavBar() {
     const navigate = useNavigate();
 
-    return(
-        <div id="navbar" onClick={() => {navigate("/");}}>
-            <img id="website--icon" src={saeIcon} onClick={() => {navigate("/");}}/>
-            <h2 id="website--title" onClick={() => {navigate("/");}}>SAE Race Data Visualizer</h2>
-        </div> 
+    const divStyle: React.CSSProperties = {
+        cursor: 'pointer', // Set cursor to hand pointer
+        width: '30%',
+        borderRadius: '6px',
+        backgroundColor: 'lightblue',
+    };
+
+    return (
+        <div id="navbar" onClick={() => { navigate("/"); }} style={divStyle}>
+            <img id="website--icon" src={saeIcon} onClick={() => { navigate("/"); }} />
+            <h2 id="website--title" onClick={() => { navigate("/"); }}>SAE Race Data Visualizer</h2>
+        </div>
     )
 }
