65f3f19822a38f38689f9ead85194e8e0361c934
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-03-22 13:46:57 -0700

    placed upload race button inside of racehistory--body

--- client/src/pages/Home/Components/UploadButton.tsx
+++ client/src/pages/Home/Components/UploadButton.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFileArrowUp} from "@fortawesome/free-solid-svg-icons";
 export default function UploadButton(){
     return(
-        <Link to="upload" style={{display:"flex", color:"black", alignItems:"center", marginLeft:"auto"}}>
+        <Link to="upload" id="upload--button">
             <h4>Upload Race</h4>
             <FontAwesomeIcon size="2x" style={{color:"black", marginLeft:"10", scale:"75%"}} icon={faFileArrowUp} />
         </Link>
