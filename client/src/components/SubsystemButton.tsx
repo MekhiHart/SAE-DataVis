e5cd91df5e22556914fd29584d464f4814cf31fc
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-04-11 12:36:57 -0700

    Fixed SubsystemButtons in ipad width

--- client/src/components/SubsystemButton.tsx
+++ client/src/components/SubsystemButton.tsx
@@ -19,7 +19,7 @@ export const SubsystemButton = (props: {subsystemName: string, onClick: React.Di
     return(
       <div className="flex subsystem--button" style={{backgroundColor: isClicked? "#98CCFC" : "#def0ff"}} onClick={() => setIsClicked(prev => !prev)}>
         {isClicked ? <FontAwesomeIcon id="subsystem--check--icon" icon={faCheck}/> : <SubsystemIcon subsystemName={subsystemName}/>}
-        <h4 style={{marginLeft:"10px"}}>{subsystemName}</h4>
+        <h4 className="subsystem--button--name" >{subsystemName}</h4>
       </div>
     )
   }
